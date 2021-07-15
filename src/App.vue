6bb3d49842eed6e651e15bdf0f053354c0c20032
tya6ivue <nishant_tyagi@excellencetechnologies.in>
2021-07-15 12:22:55 +0530

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InputForm />
+    
+     <!-- <Assignment /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import InputForm from './components/InputForm.vue'
+// import Assignment from './components/Assignment.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputForm,
+    
+    // Assignment
   }
 }
 </script>
